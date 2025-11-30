b72c9da200e2a402fad4dd1ce8a65c5991244a91
kavyaa04 <128207728+kavyaa04@users.noreply.github.com>
2025-11-30 12:05:22 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,13 @@
+#wap to find area and circumference of circle with radius 39cm
+
+radius=39
+pi= 3.14
+
+area = pi*(radius**2)
+print(f"Area: {area:.2f}")
+
+circumference = 2*pi*radius
+print(f"Circumference: {circumference:.2f}")
+
+
+
