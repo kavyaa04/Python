c00dd27bb03ac29536b2f38b3f0b6cc15e77f674
kavyaa04 <128207728+kavyaa04@users.noreply.github.com>
2025-12-01 10:49:48 +0530

    Add files via upload

---
+++ formatting_string.py
@@ -0,0 +1,15 @@
+#FORMATTING STRINGS
+
+player, country, position= "virat", "India", "batsman"
+print(f"The player {player:<12} plays for {country:^12} as a {position:>12} in the team")
+
+player, country, position= "Manbabwalu", "Zimbawae", "wicket-keeper"
+print(f"The player {player:<12} plays for {country:^12} as a {position:>12} in the team")
+
+
+
+
+
+
+
+
