a652bcfddfbfd2dc9352dd4602f01dd8b5f5880a
kavyaa04 <128207728+kavyaa04@users.noreply.github.com>
2025-12-02 21:28:58 +0530

    Add files via upload

---
+++ ip_frm_users.py
@@ -0,0 +1,17 @@
+#Area and Perimater of rectangle
+
+
+l= float(input("Enter the length: "))
+b= int(input("Enter the breadth: "))
+
+area= l*b
+
+print(f"the area of reactangle is {area} ")
+
+
+
+
+
+
+
+
